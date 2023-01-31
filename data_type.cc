fa72d5fdafe65f3024ab64756a907caa29432d77
Kininaru <shiftregister233@outlook.com>
2023-01-31 13:06:59 +0800

    Create data_type.cc

---
+++ data_type.cc
@@ -0,0 +1,12 @@
+#define u8 unsigned char
+#define u16 unsigned short
+#define u32 unsigned long
+#define u64 unsigned long long
+
+#define i8 char
+#define i16 short
+#define i32 long
+#define i64 long long
+
+#define f32 float
+#define f64 double
