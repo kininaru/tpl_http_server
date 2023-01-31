904b9125ea373af600b92e8c461f37490c1040e9
Kininaru <shiftregister233@outlook.com>
2023-01-31 13:49:45 +0800

    add json support

--- controller/default.cc
+++ controller/default.cc
@@ -4,13 +4,25 @@
 #include "workflow/WFHttpServer.h"
 #include "workflow/WFServer.h"
 
+#include "nlohmann/json.hpp"
+
 #include "../define.cc"
 
+using json = nlohmann::json;
+
+void Response(RESPONSE *res, int code, std::string msg) {
+    json body = {
+        {"code", code},
+        {"msg", msg}
+    };
+    res->append_output_body(body.dump());
+}
+
 void ControllerIndex(REQUEST *req, RESPONSE *res) {
-    res->append_output_body("this is index");
+    Response(res, 0, "here is index");
 }
 
 void ControllerNotFound(REQUEST *req, RESPONSE *res) {
     res->set_status_code("404");
-    res->append_output_body("api not found");
+    Response(res, 404, "page not found");
 }
